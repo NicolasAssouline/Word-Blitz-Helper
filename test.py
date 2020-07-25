0e6f96b91c6f78aca2b1c1014689bd6c970bf610
nicolas <nicolas.assouline@fer.hr>
2020-07-25 11:21:48 +0200

    Rectangle works

--- test.py
+++ test.py
@@ -1,7 +1,7 @@
 import sys
 
-from PyQt5 import QtCore
-from PyQt5.QtCore import Qt
+from PyQt5 import QtCore, QtGui
+from PyQt5.QtCore import Qt, QRect
 from PyQt5 import QtWidgets
 from PyQt5.QtGui import QPainter, QBrush
 from PyQt5.QtWidgets import QMainWindow, QApplication
@@ -26,10 +26,19 @@ class MainWindow(QMainWindow):
 		))
 		QMainWindow.setWindowOpacity(self, 0.5)
 
+		self.mouse_start_pos = None
+		self.mouse_curr_pos = None
 		print('Done')
 
+	def mouseReleaseEvent(self, event: QtGui.QMouseEvent) -> None:
+		self.mouse_start_pos = None
+		QtWidgets.qApp.quit()
 
-	def mousePressEvent(self, event):
+	def mousePressEvent(self, event: QtGui.QMouseEvent):
+		if self.mouse_start_pos is None:
+			self.mouse_start_pos = event.pos()
+
+
 		# painter = QPainter(self)
 		# painter.setBrush()
 		# painter.setPen(QPen(Qt.black, 2, Qt.SolidLine))
@@ -38,17 +47,30 @@ class MainWindow(QMainWindow):
 
 		print('mouseclick')
 		# time.sleep(4)
-		QtWidgets.qApp.quit()
+		# QtWidgets.qApp.quit()
 
-	def paintEvent(self, event) -> None:
+	def mouseMoveEvent(self, event: QtGui.QMouseEvent) -> None:
+		if self.mouse_start_pos is None:
+			self.mouse_start_pos = event.pos()
+		self.mouse_curr_pos = event.pos()
+		self.update()
+
+	def paintEvent(self, event: QtGui.QPaintEvent) -> None:
 		qp = QPainter()
 		qp.begin(self)
-		# qp.drawLine(200, 200, 400, 400)
-		qp.fillRect(100, 15, 800, 200, qp.brush())
-		qp.drawRect(100, 15, 800, 600)
+
+		if self.mouse_start_pos is not None and self.mouse_curr_pos is not None:
+			qp.drawRect(QRect(self.mouse_start_pos.x(), self.mouse_start_pos.y(),
+							  # self.mouse_start_pos.x()+1, self.mouse_start_pos.y()+1))
+							  self.mouse_curr_pos.x()-self.mouse_start_pos.x(),
+							  self.mouse_curr_pos.y()-self.mouse_start_pos.y()))
+
+			print(self.mouse_start_pos.x(), self.mouse_start_pos.y(),
+				   self.mouse_curr_pos.x(), self.mouse_curr_pos.y())
 		qp.end()
 
-		print('paintevent')
+
+		# print('paintevent')
 		# time.sleep(4)
 		# QtWidgets.qApp.quit()
 
