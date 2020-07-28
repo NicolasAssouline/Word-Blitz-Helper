1643e1111cf00a29a2e1bb9906cbe7df638a766c
nicolas <nicolas.assouline@fer.hr>
2020-07-28 12:07:06 +0200

    More cleanup

--- test2.py
+++ test2.py
@@ -16,7 +16,7 @@ from utils import analyze
 #  - cell_threshold
 #  - min_columns
 
-def pre_process_image(img, save_in_file, morph_size=(23, 23)):
+def pre_process_image(img, morph_size=(23, 23)):
 	# get rid of the color
 	pre = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 	# Otsu threshold
@@ -25,11 +25,7 @@ def pre_process_image(img, save_in_file, morph_size=(23, 23)):
 	cpy = pre.copy()
 	struct = cv2.getStructuringElement(cv2.MORPH_RECT, morph_size)
 	cpy = cv2.dilate(~cpy, struct, anchor=(-1, -1), iterations=1)
-	pre = ~cpy
-
-	if save_in_file is not None:
-		cv2.imwrite(save_in_file, pre)
-	return pre
+	return ~cpy
 
 
 def find_text_boxes(pre, min_text_height_limit=40, max_text_height_limit=120):
@@ -46,60 +42,6 @@ def find_text_boxes(pre, min_text_height_limit=40, max_text_height_limit=120):
 			boxes.append(box)
 
 	return boxes
-
-
-def find_table_in_boxes(boxes, cell_threshold=100, min_columns=3):
-	rows = {}
-	cols = {}
-
-	# Clustering the bounding boxes by their positions
-	for box in boxes:
-		(x, y, w, h) = box
-		col_key = x // cell_threshold
-		row_key = y // cell_threshold
-		cols[row_key] = [box] if col_key not in cols else cols[col_key] + [box]
-		rows[row_key] = [box] if row_key not in rows else rows[row_key] + [box]
-
-	# Filtering out the clusters having less than 2 cols
-	table_cells = list(filter(lambda r: len(r) >= min_columns, rows.values()))
-	# Sorting the row cells by x coord
-	table_cells = [list(sorted(tb)) for tb in table_cells]
-	# Sorting rows by the y coord
-	table_cells = list(sorted(table_cells, key=lambda r: r[0][1]))
-
-	return table_cells
-
-
-def build_lines(table_cells):
-	if table_cells is None or len(table_cells) <= 0:
-		return [], []
-
-	max_last_col_width_row = max(table_cells, key=lambda b: b[-1][2])
-	max_x = max_last_col_width_row[-1][0] + max_last_col_width_row[-1][2]
-
-	max_last_row_height_box = max(table_cells[-1], key=lambda b: b[3])
-	max_y = max_last_row_height_box[1] + max_last_row_height_box[3]
-
-	hor_lines = []
-	ver_lines = []
-
-	for box in table_cells:
-		x = box[0][0]
-		y = box[0][1]
-		hor_lines.append((x, y, max_x, y))
-
-	for box in table_cells[0]:
-		x = box[0]
-		y = box[1]
-		ver_lines.append((x, y, x, max_y))
-
-	(x, y, w, h) = table_cells[0][-1]
-	ver_lines.append((max_x, y, max_x, max_y))
-	(x, y, w, h) = table_cells[0][0]
-	hor_lines.append((x, max_y, max_x, max_y))
-
-	return hor_lines, ver_lines
-
 
 if __name__ == "__main__":
 	in_file = '/home/nicolas/Pictures/Screenshot from 2020-07-25 13-28-19.png'
@@ -146,6 +88,8 @@ if __name__ == "__main__":
 
 			print(detected, end=' ')
 		print()
+
+	solve_blitz()
 
 	# print('boxes:', len(text_boxes))
 	# for i, cell in enumerate(text_boxes):
